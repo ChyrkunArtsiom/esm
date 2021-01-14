5056ffb20e336078788cdee0ba7a60e0c9e79738
Artsiom Chyrkun <chyrkun.artsiom@gmail.com>
2021-01-14 11:14:48 +0300

    TestConfiguration with datasource bean. Sql scripts only in test/resources.

--- persistence/src/test/java/com/epam/esm/dao/impl/TagDAOTest.java
+++ persistence/src/test/java/com/epam/esm/dao/impl/TagDAOTest.java
@@ -39,7 +39,7 @@ class TagDAOTest {
     }
 
     @Test
-    @Sql({"classpath:data.sql"})
+/*    @Sql({"classpath:data.sql"})*/
     public void testReadByName() {
         Tag tag = dao.read("thirdtag");
         assertNotNull(tag);
